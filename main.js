e8e3d72172a503d49c3bcf4e824b70cda75a295d
DIperera <IT23590762@my.sliit.lk>
2024-11-30 11:20:44 +0530

    Update item classes

--- main.js
+++ main.js
@@ -119,6 +119,82 @@ function page() {
     window.location.href = "movie.html";
 }
 
+function centerDiv(button) {
+    const div = button.parentElement;
+    const value = div.getAttribute("data-value");
+
+    div.classList.add('active');
+
+    if (value === "1") {
+        div.innerHTML = `
+            <iframe 
+                src="https://www.youtube.com/embed/pzD9zGcUNrw?si=mDyx5hfKFeejFm9I" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe><br><button onclick="skip()">Back</button>`;
+    } else if (value === "2") {
+        div.innerHTML = `
+            <iframe 
+                src="https://www.youtube.com/embed/aM9OvISC1Dg?si=aoGPQNSh-ZUmrbm-" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe><br><button onclick="skip()">Back</button>`;
+    } else if (value === "3") {
+        div.innerHTML = `
+            <iframe 
+                src="https://www.youtube.com/embed/ejMMn0t58Lc?si=UZkchKg9bT1P2kgY" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe><br><button onclick="skip()">Back</button>`;
+    } else if (value === "4") {
+        div.innerHTML = `
+            <iframe 
+                src="https://www.youtube.com/embed/VFsmuRPClr4?si=mptIW_kM8uyXrGjg" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe><br><button onclick="skip()">Back</button>`;
+    } else if (value === "5") {
+        div.innerHTML = `
+            <iframe 
+                src="https://www.youtube.com/embed/fChx_YZUAR0?si=J-ZMZdaTI9MRIMnb" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe><br><button onclick="skip()">Back</button>`;
+    } else if (value === "6") {
+        div.innerHTML = `
+            <iframe 
+                src="https://www.youtube.com/embed/BcDK7lkzzsU?si=-nB6w_2wbcOhZjVk" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe><br><button onclick="skip()">Back</button>`;
+    } 
+
+    // Scroll to the top
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+}
+
+function skip(){
+    location.reload();
+}
 /*const horror = document.getElementById("horror").innerHTML;
 const horrormovies = `<h3 style="color:red">Latest horror movies...</h3><br>`+ horror;//here only ` can be used*/
 
